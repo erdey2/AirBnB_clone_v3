eda6cfeedf515969afd99cba2b65b158a3efd286
erdey syoum <erdeysyoum@gmail.com>
2024-04-29 05:22:21 -0700

    bare model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,19 +65,6 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
-    def get(self, cls, id):
-        """get an object"""
-        if cls and id:
-            copy = '{}.{}'.format(cls, id)
-            instance = self.all(cls)
-            return instance.get(copy)
-        else:
-            return None
-
-    def count(self, cls=None):
-        """count objects"""
-        return (len(self.all(cls)))
-
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
